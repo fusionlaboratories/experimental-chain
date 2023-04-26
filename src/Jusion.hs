ae6384d2279db28869aa1f9930cc29cf1a20c1f9
Qredo LTD <support@qredo.com>
2023-04-26 12:23:17 +0200

    WIP DSL

--- src/Jusion.hs
+++ src/Jusion.hs
@@ -1,3 +1,31 @@
+{-# LANGUAGE DataKinds #-}
 module Jusion where
 
+import Data.FiniteField.PrimeField
+
+-- A simple prime field
+type F17 = PrimeField 17
+
 -- going for a super simple implementation of the AST
+data BinOp 
+    -- Basic arithmetic
+    = Add
+    | Mul
+    -- Equality check
+    | Eq
+
+data UnOp
+    -- Unary operations
+    = Inv
+    | Neg
+
+data Exp
+    = Lit F17
+    | BinOp BinOp Exp Exp
+    | UnOp UnOp Exp
+
+newtype Stmt
+    = Assert Exp
+
+newtype Program
+    = Program [Stmt]
--- tests/test.hs
+++ tests/test.hs
@@ -3,4 +3,12 @@ module Main where
 import Test.Tasty
 import Test.Tasty.HUnit
 
-main = defaultMain $ testCase "foo" $ 1 @?= 1
+import Jusion
+
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+    testGroup "finite fields" [
+        testCase "f17 wraps around" $ (1 :: F17) @?= (18 :: F17)
+    ]
