ff27c5a423830e706f44c4792698a5146b9f9c39
Qredo LTD <support@qredo.com>
2023-05-08 22:35:05 +0200

    Clean flake and add miden

--- flake.nix
+++ flake.nix
@@ -3,14 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     devenv.url = "github:cachix/devenv";
+    miden-vm.url = "github:qredek/miden-vm/add-exitcodes";
+    miden-vm.flake = false;
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devenv.flakeModule
       ];
+
+      debug = true;
 
       # systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       systems = [ "aarch64-darwin" ];
@@ -20,18 +25,50 @@
         # module parameters provide easy access to attributes of the same
         # system.
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        # packages.default = pkgs.hello;
+        _module.args.pkgs = import self.inputs.nixpkgs {
+          inherit system;
+          overlays = [(import self.inputs.rust-overlay)];
+        };
+
+        # Latest Rust toolchain
+        packages.rust = pkgs.rust-bin.stable.latest.default;
+
+        # Miden VM
+        packages.miden = pkgs.rustPlatform.buildRustPackage {
+          pname = "miden-vm";
+          version = "0.5.0";
+          src = self.inputs.miden-vm;
+          buildType = "release";
+          buildFeatures = ["executable" "concurrent"];
+          nativeBuildInputs = [self'.packages.rust];
+          doCheck = false;
+          cargoLock = {
+            lockFile = "${self.inputs.miden-vm}/Cargo.lock";
+          };
+        };
+
+        # Haskell Toolchain
+        packages.ghc = pkgs.ghc;
+        packages.haskell-language-server = pkgs.haskell-language-server;
 
         devenv.shells.default = {
           name = "jusion";
 
           # https://devenv.sh/reference/options/
-          # packages = []
+          packages = [
+            # Miden
+            self'.packages.miden
+
+            # Haskell
+            self'.packages.ghc
+            self'.packages.haskell-language-server
+            pkgs.cabal-install
+            pkgs.zlib
+          ];
+
+          # Extra languages
           languages = {
             javascript.enable = true;
-            haskell.enable = true;
-            rust.enable = true;
           };
         };
       };
