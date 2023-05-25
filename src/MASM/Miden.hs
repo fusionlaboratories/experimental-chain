27af207b57ca23b33b8948c469ed756fb86308dc
Qredo LTD <support@qredo.com>
2023-05-25 14:44:18 +0200

    be explicit about ignoring data

--- src/MASM/Miden.hs
+++ src/MASM/Miden.hs
@@ -24,7 +24,7 @@ runMiden :: KeepFile -> Module -> IO (Either String [Word32])
 runMiden keep m = withSystemTempFile "starkify-testfile-XXX.masm" $ \fp hndl -> do
     hPutStrLn hndl (ppMASM m)
     hClose hndl
-    whenKeep keep $ \masmModSaveFp -> copyFile fp masmModSaveFp
+    _ <- whenKeep keep $ \masmModSaveFp -> copyFile fp masmModSaveFp
     (ex, midenout, midenerr) <- readProcessWithExitCode "miden" ["run", "--assembly", fp] ""
     case ex of
         ExitSuccess -> do
