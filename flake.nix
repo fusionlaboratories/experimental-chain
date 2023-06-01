5855f9dbd7d9ccc059dc9563690dc43fb27215e7
Qredo LTD <support@qredo.com>
2023-06-01 18:13:39 +0200

    Add jusion-substrate-node

--- flake.nix
+++ flake.nix
@@ -31,7 +31,9 @@
         };
 
         # Latest Rust toolchain
-        packages.rust = pkgs.rust-bin.stable.latest.default;
+        packages.rust = pkgs.rust-bin.stable.latest.default.override {
+          targets = ["wasm32-unknown-unknown"];
+        };
 
         # Miden VM
         packages.miden = pkgs.rustPlatform.buildRustPackage {
